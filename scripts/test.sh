c350ee245415fe55d022e32c197014a70d1c10b4
Patrick DURAND <patrick@plealog.com>
2018-08-31 14:32:24 +0200

    add dumpcsv.sh to the test suite

--- scripts/test.sh
+++ scripts/test.sh
@@ -53,8 +53,8 @@ if [  ! "$KL_WORKING_DIR"  ]; then
   exit 1
 fi
 
-# *** TEST 1/4: index a sequence file
-echo "> 1/4: Running 'index.sh' tool:"
+# *** TEST 1/5: index a sequence file
+echo "> 1/5: Running 'index.sh' tool:"
 if [ -d $KL_WORKING_DIR/$INDEX_DIR/${IDX_NAME}  ]; then
   echo "   skip: already done"
 else
@@ -69,8 +69,8 @@ else
   fi
 fi
 
-# *** TEST 2/4: query the created index
-echo "> 2/4: Running 'query.sh' tool:"
+# *** TEST 2/5: query the created index
+echo "> 2/5: Running 'query.sh' tool:"
 CMD="$KL_APP_HOME/query.sh -w $KL_WORKING_DIR -d $KL_WORKING_DIR/$INDEX_DIR/${IDX_NAME} -i ENSTTRP00000003887,ENSTTRP00000007204,ENSTTRP00000007202 > $KL_WORKING_DIR/bdm-query-test.fa"
 echo "$CMD"
 eval $CMD
@@ -81,7 +81,7 @@ else
   echo "  OK"
 fi
 
-# *** TEST 3/4: cut a sequence into slices of 1000 sequences each
+# *** TEST 3/5: cut a sequence into slices of 1000 sequences each
 # Prepare a symlink to the file to cut: it can be located in a place
 # having only read access... and the 'cut' tool writes new files next
 # to input file
@@ -93,7 +93,7 @@ if [ ! -L "$KL_WORKING_DIR/$SEQ_NAME" ]; then
   fi
 fi
 
-echo "> 3/4 Running 'cut.sh' tool:"
+echo "> 3/5 Running 'cut.sh' tool:"
 CMD="$KL_APP_HOME/cut.sh -w $KL_WORKING_DIR -i $KL_WORKING_DIR/$SEQ_NAME -p 1000"
 echo "$CMD"
 eval $CMD
@@ -104,8 +104,8 @@ else
   echo "  OK"
 fi
 
-# *** TEST 4/4: run PLASTp
-echo "> 4/4 Running 'plast.sh' tool:"
+# *** TEST 4/5: run PLASTp
+echo "> 4/5 Running 'plast.sh' tool:"
 export KL_WORKING_DIR=$KL_WORKING_DIR
 CMD="$KL_APP_HOME/plast.sh -p plastp -i $KL_APP_HOME/data/query.fa -d $KL_APP_HOME/data/tursiops.fa -o $KL_WORKING_DIR/bdm-tools-plast.xml -a 8 -maxhits 5 -maxhsps 1 -e 1e-5 -F F"
 echo "$CMD"
@@ -117,4 +117,15 @@ else
   echo "  OK"
 fi
 
+# *** TEST 5/5: run CSV export
+echo "> 5/5 Running 'dumpcsv.sh' tool:"
+CMD="$KL_APP_HOME/dumpcsv.sh -i $KL_WORKING_DIR/bdm-tools-plast.xml -o $KL_WORKING_DIR/bdm-tools-plast.csv -bho"
+echo "$CMD"
+eval $CMD
+if [ $? != 0  ]; then
+  error "unable to run 'dumpcsv.sh'"
+  exit 1
+else
+  echo "  OK"
+fi
 
